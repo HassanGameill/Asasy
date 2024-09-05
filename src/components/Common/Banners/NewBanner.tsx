713a11b681a54075578341a95c90a52fbc2589ad
HassanGameill <hasanaboalion@gmail.com>
2024-09-05 20:01:04 +0300

    update commit

--- src/components/Common/Banners/NewBanner.tsx
+++ src/components/Common/Banners/NewBanner.tsx
@@ -7,9 +7,9 @@ function NewBanner() {
   return (
     <div className="bg-green-400   flex flex-col lg:flex-row justify-between items-center gap-4 shadow-lg rounded-md p-5 px-10  ">
 
-      <div className="py-5 lg:w-[600px]">
+      <div className="py-5 lg:w-[600px] bg-black/60 p-4 rounded-lg">
         <h2 className="font-semibold text-2xl text-white">
-          Asasy Package <span className="text-orange-400">Famly House</span>{" "}
+          Asasy Package <span className="text-[#FFD500]">Famly House</span>{" "}
         </h2>
         <p className="text-[13px] mt-3 overflow-hidden text-white">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus officia
